3bf531f85f8dfc4383469cd6b0377a47ff5ea235
felipeZ <tifonzafel@gmail.com>
2018-05-01 18:41:45 +0200

    first commit

---
+++ include/gaussFunctions.h
@@ -0,0 +1,23 @@
+#include <string>
+
+/*! Orbital labels  */
+enum class OrbitalType: unsigned char
+{
+  S,
+  P,
+  D,
+  F
+};
+
+
+/* class CGF */
+/* { */
+  
+  
+/* } */
+
+
+/* class Gaussian */
+/* { */
+  
+/* } */
