df141a7d010ab05d8db58c66efb80ba44781a544
felipeZ <tifonzafel@gmail.com>
2018-06-01 00:27:49 +0200

    reading data from HDF5

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 
-#include <iostream>
 #include <armadillo>
+#include <iostream>
+#include <string>
+#include <vector>
 #include "H5Cpp.h"
-using namespace h5;
 
-using namespace arma;
-using namespace std;
+// using namespace arma;
+// using namespace std;
 
-// using arma::mat;
-// using arma::randu;
+using namespace H5;
+using arma::mat;
+using arma::randu;
+using std::cout;
+using std::endl;
+
+
+int read_data_from_hdf5(const std::string& path_file, const std::string& path_dataset)
+{
+  H5::H5File file_handler(path_file, H5F_ACC_RDONLY);
+  H5::DataSet dataset = file_handler.openDataSet(path_dataset);
+  H5T_class_t type_class = dataset.getTypeClass();
+  file_handler.close();
+  
+  cout << "Data store in the file: " << type_class << endl;
+  return(0);
+}
 
 auto main() ->  int
 {
@@ -17,5 +33,10 @@ auto main() ->  int
 
   cout << "result: " << (arr * brr) << endl;
 
+  auto path_dataset = "ethylene/point_0/cp2k/mo/eigenvalues";
+  auto path_file = "/home/felipe/Primer/insilico/data/ethylene.hdf5";
+
+  int val = read_data_from_hdf5(path_file, path_dataset);
+  
   return(0);
 }
