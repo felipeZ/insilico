22d07ab191003a7247f0acdb3c7f15753dd52c58
felipe <tifonzafel@gmail.com>
2018-09-28 11:35:07 +0200

    added namespace

--- include/delay.h
+++ include/delay.h
@@ -1,6 +1,8 @@
 #ifndef INSILICO_DELAY_H_
 #define INSILICO_DELAY_H_
 
+namespace insilico {
+
 template<class T> class Delay {
  private:
   function<T()> m_func;
@@ -12,5 +14,7 @@ template<class T> class Delay {
     return m_func();
   }
 };
+
+} // end namespace
 
 #endif // INSILICO_DELAY_H_
