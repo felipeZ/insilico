ca99d29eae68ed5535b4bea95e45a4351a0c9ba9
felipez <tifonzafel@gmail.com>
2019-03-27 22:10:30 +0100

    cleaning old ideas

---
+++ include/predictor.hpp
@@ -0,0 +1,18 @@
+#ifndef INSILICO_PREDICTOR_H_
+#define INSILICO_PREDICTOR_H_
+
+#include <Eigen/Dense>
+
+namespace insilico {
+
+// class  Predictor {
+
+// private:
+  
+// public:
+
+//   ()
+// }
+
+}  
+#endif // INSILICO_PREDICTOR_H_
--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "funtools.h"
 
 
 using std::cout;
